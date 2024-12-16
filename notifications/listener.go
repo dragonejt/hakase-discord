d93e91e90f5fd02e6bb6e480c96757f8db08cb71
Evan Tung <dragonejt@gmail.com>
2024-12-16 00:24:06 -0500

    don't return errors in goroutines

--- notifications/listener.go
+++ notifications/listener.go
@@ -6,33 +6,32 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func StartListening(rabbitMQURL string, queueName string) error {
+func StartListening(rabbitMQURL string, queueName string) {
 	connection, err := amqp.Dial(rabbitMQURL)
 	if err != nil {
-		return err
+		slog.Error("error starting rabbitMQ connection: " + err.Error())
+		return
 	}
 	defer connection.Close()
 
 	channel, err := connection.Channel()
 	if err != nil {
-		return err
+		slog.Error("error initializing connection channel: " + err.Error())
 	}
 	defer channel.Close()
 
 	queue, err := channel.QueueDeclare(queueName, false, false, false, false, nil)
 	if err != nil {
-		return err
+		slog.Error("error creating channel queue: " + err.Error())
 	}
 
 	notifications, err := channel.Consume(queue.Name, "", true, false, false, false, nil)
 	if err != nil {
-		return err
+		slog.Error("error consuming notifications: " + err.Error())
 	}
 
 	for n := range notifications {
 		slog.Info("received notification" + string(n.Body))
 	}
-
-	return nil
 
 }
